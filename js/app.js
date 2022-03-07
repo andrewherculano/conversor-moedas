350660229ba66181777d233db2ded822a4e11ba2
andrew herculano <andrewherculano@hotmail.com>
2022-03-07 16:45:21 -0300

    obtendo informações das moedas e tratando erros

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const url = 'https://v6.exchangerate-api.com/v6/5c8d0f7db23a584339f95428/latest/USD'
+
+const getErrorMessage = (error) => {
+  return {
+    'unsupported-code': 'A moeda não existe no banco de dados',
+    'malformed-request': 'O endpoint informado não é válido',
+    'invalid-key': 'A chave da API não é válida',
+    'inactive-account': 'Endereço de e-mail não confirmado',
+    'quota-reached': 'O limite de solicitações permitidas foi atingido'
+  }[error] || 'Não foi possível obter os dados'
+}
+
+const fetchExchangeRate = async () => {
+  try {
+    const response = await fetch(url)
+    const exchangeRateData = await response.json()
+
+    if (exchangeRateData.result === 'error') {
+      const errorType = getErrorMessage(exchangeRateData['error-type'])
+      throw new Error(errorType)
+    }
+
+    console.log(exchangeRateData)
+  } catch ({ message }) {
+    console.log(message)
+  }
+}
+
+fetchExchangeRate()
