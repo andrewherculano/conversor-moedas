a90f1ff109bc861c244725c66046d8b667056c30
andrew herculano <andrewherculano@hotmail.com>
2022-03-09 14:52:56 -0300

    implementando logica do calculo para a conversão das moedas

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ const alertErrorMessageEl = document.querySelector('[data-js="alert-error-messag
 const alertErrorButtonEl = document.querySelector('[data-js="alert-error-button"]')
 const currencyOneEl = document.querySelector('[data-js="currency-one"]')
 const currencyTwoEl = document.querySelector('[data-js="currency-two"]')
+const conversionResultEl = document.querySelector('[data-js="conversion-result"]')
 const conversionPresicionEl = document.querySelector('[data-js="conversion-precision"]')
+const amountToConversionEl = document.querySelector('[data-js="amount-to-conversion"]')
 
 let internalExchangeRate = {}
 
@@ -46,7 +48,7 @@ const fetchExchangeRate = async (url) => {
 }
 
 const initializer = async () => {
-  internalExchangeRate = { ...await fetchExchangeRate(getUrl('USD')) }
+  internalExchangeRate = { ...(await fetchExchangeRate(getUrl('USD'))) }
 
   const getOptions = (selectedCurrency) => {
     return Object.keys(internalExchangeRate.conversion_rates)
@@ -58,5 +60,21 @@ const initializer = async () => {
 
   conversionPresicionEl.textContent = `1 USD igual a ${internalExchangeRate.conversion_rates[currencyTwoEl.value].toFixed(2)} BRL`
 }
+
+amountToConversionEl.addEventListener('input', (e) => {
+  conversionResultEl.textContent = (e.target.value * internalExchangeRate.conversion_rates[currencyTwoEl.value]).toFixed(2)
+})
+
+currencyOneEl.addEventListener('input', async (e) => {
+  internalExchangeRate = { ...(await fetchExchangeRate(getUrl(e.target.value))) }
+
+  conversionResultEl.textContent = (amountToConversionEl.value * internalExchangeRate.conversion_rates[currencyTwoEl.value]).toFixed(2)
+  conversionPresicionEl.textContent = `1 ${e.target.value} igual a ${internalExchangeRate.conversion_rates[currencyTwoEl.value].toFixed(2)} ${currencyTwoEl.value}`
+})
+
+currencyTwoEl.addEventListener('input', (e) => {
+  conversionResultEl.textContent = (amountToConversionEl.value * internalExchangeRate.conversion_rates[e.target.value]).toFixed(2)
+  conversionPresicionEl.textContent = `1 ${currencyOneEl.value} igual a ${internalExchangeRate.conversion_rates[currencyTwoEl.value].toFixed(2)} ${currencyTwoEl.value}`
+})
 
 initializer()
